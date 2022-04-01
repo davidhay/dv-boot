aaeb0b3ada6de983eb59bfbf11096142f6951deb
David Hay <david.j.hay@gmail.com>
2022-04-01 15:54:05 +0100

    modify the LogoutListener to add the sessionId (if available) to the CreateClientEvent to support testing

--- datavault-webapp/src/main/java/org/datavaultplatform/webapp/controllers/LogoutListener.java
+++ datavault-webapp/src/main/java/org/datavaultplatform/webapp/controllers/LogoutListener.java
@@ -1,6 +1,7 @@
 package org.datavaultplatform.webapp.controllers;
 
 import java.util.List;
+import javax.servlet.http.HttpSession;
 import org.datavaultplatform.common.request.CreateClientEvent;
 import org.datavaultplatform.webapp.services.NotifyLogoutService;
 import org.slf4j.Logger;
@@ -11,6 +12,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.session.SessionDestroyedEvent;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.session.HttpSessionDestroyedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -38,7 +40,16 @@ public class LogoutListener implements ApplicationListener<SessionDestroyedEvent
             WebAuthenticationDetails details = (WebAuthenticationDetails)auth.getDetails();
             String remoteAddress = details.getRemoteAddress();
             CreateClientEvent clientEvent = new CreateClientEvent(remoteAddress, null);
-            
+
+            //If available, add the sessionId to the clientEvent - this is just to aid testing
+            if (event instanceof HttpSessionDestroyedEvent) {
+                HttpSessionDestroyedEvent httpSessionEvent = (HttpSessionDestroyedEvent) event;
+                HttpSession session = httpSessionEvent.getSession();
+                if (session != null) {
+                    clientEvent.setSessionId(session.getId());
+                }
+            }
+
             // Log the event with the broker
             try {
                 //TODO - this method returns a string which we are ignoring
