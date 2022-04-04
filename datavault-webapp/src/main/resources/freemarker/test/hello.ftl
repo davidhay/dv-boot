232d12b81cd32aaf166fa4dc1762e8847088add4
David Hay <david.j.hay@gmail.com>
2022-04-04 11:20:57 +0100

    The links in the hello.ftl template were wrong

--- datavault-webapp/src/main/resources/freemarker/test/hello.ftl
+++ datavault-webapp/src/main/resources/freemarker/test/hello.ftl
@@ -1,12 +1,13 @@
 <!DOCTYPE html><!--hello.ftl-->
+<#import "/spring.ftl" as spring />
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <title>Hello ${name}!</title>
-  <link href="resources/test/css/hello.css" rel="stylesheet">
+  <link href="<@spring.url '/resources/test/css/hello.css'/>" rel="stylesheet" type="text/css">
 </head>
 <body>
 <h2 class="hello-title">Hello ${name}!</h2>
-<script src="resources/test/js/hello.js"></script>
+<script src="<@spring.url '/resources/test/js/hello.js'/>"></script>
 </body>
 </html>
--- datavault-webapp/src/test/java/org/datavaultplatform/webapp/app/setup/HelloControllerTest.java
+++ datavault-webapp/src/test/java/org/datavaultplatform/webapp/app/setup/HelloControllerTest.java
@@ -1,20 +1,28 @@
 package org.datavaultplatform.webapp.app.setup;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import lombok.extern.slf4j.Slf4j;
 import org.datavaultplatform.webapp.test.AddTestProperties;
 import org.hamcrest.Matchers;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @AddTestProperties
+@Slf4j
 public class HelloControllerTest {
 
   @Autowired
@@ -23,9 +31,30 @@ public class HelloControllerTest {
   @Test
   void testHelloController() throws Exception {
     String id = "xyz123";
-    mvc.perform(get("/test/hello?name=" + id))
+    MvcResult result = mvc.perform(get("/test/hello?name=" + id))
         .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
         .andExpect(MockMvcResultMatchers.content().string(
-            Matchers.containsString(id)));
+            Matchers.containsString(id)))
+        .andReturn();
+
+    Document doc = Jsoup.parse(result.getResponse().getContentAsString());
+    Element cssElem = doc.selectFirst("link");
+    String cssUrl = cssElem.attr("href");
+    log.info("css url", cssUrl);
+    checkLink(cssUrl, "text/css;charset=UTF-8");
+
+    Element jsElem = doc.selectFirst("script");
+    String jsUrl = jsElem.attr("src");
+    log.info("js url", jsUrl);
+    checkLink(jsUrl,"application/javascript;charset=UTF-8");
   }
+
+  MvcResult checkLink(String url, String contentType) throws Exception {
+    log.info("url {}", url);
+    return mvc.perform(get(url))
+        .andExpect(status().is2xxSuccessful())
+        .andExpect(content().contentType(contentType))
+        .andReturn();
+  }
+
 }
