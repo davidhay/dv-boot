fb2efb21a7bcfdd3bfe7b777c8e81351ebca4181
David Hay <david.j.hay@gmail.com>
2022-04-04 16:36:05 +0100

    added ForceLogoutService

---
+++ datavault-webapp/src/main/java/org/datavaultplatform/webapp/services/ForceLogoutService.java
@@ -0,0 +1,49 @@
+package org.datavaultplatform.webapp.services;
+
+import com.google.common.collect.Sets;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ForceLogoutService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ForceLogoutService.class);
+
+    private final SessionRegistry sessionRegistry;
+
+    private final RestService supportService;
+
+    @Autowired
+    public ForceLogoutService(
+        SessionRegistry sessionRegistry,
+        RestService supportService) {
+        this.sessionRegistry = sessionRegistry;
+        this.supportService = supportService;
+    }
+
+    public void logoutUser(String userId) {
+        logger.warn("Forcing a logout for user {}", userId);
+        logoutUsers(Sets.newHashSet(userId));
+    }
+
+    public void logoutRole(long roleId) {
+        logger.warn("Forcing a logout for all users assigned to role {}", roleId);
+        Set<String> userIds = supportService.getRoleAssignmentsForRole(roleId).stream()
+                .map(roleAssignment -> roleAssignment.getUserId())
+                .collect(Collectors.toSet());
+        logoutUsers(userIds);
+    }
+
+    private void logoutUsers(Set<String> userIds) {
+        sessionRegistry.getAllPrincipals().stream()
+                .filter(principal -> principal instanceof String && userIds.contains(principal))
+                .flatMap(principal -> sessionRegistry.getAllSessions(principal, false).stream())
+                .forEach(SessionInformation::expireNow);
+    }
+}
